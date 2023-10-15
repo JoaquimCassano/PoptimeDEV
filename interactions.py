09a3515564dc87b2d6aee3663e23d78d10d61fa4
Joaquim cassano <joaquimcassano@gmail.com>
2023-10-15 13:10:19 -0300

    Error log no interactions.py, e refatoramento do GetContextOfReply

--- interactions.py
+++ interactions.py
@@ -6,21 +6,23 @@ from ai import AiResponse
 class ArvoreTweets:
     def __init__(self, parentTweet:Tweet) -> None:
         self.text = parentTweet.text
-        self.author = parentTweet.author.username
+        self.author = parentTweet.author.username #type:ignore
 
         self.replies = []
     
-    def asJson(self, tweet:ArvoreTweets|None=None) -> str: #type:ignore
+    @property
+    def asJson(self) -> str: #type:ignore
         tweetDict = {"text": self.text}
         if self.replies:
-            tweetDict["replies"] = [self.asJson(reply) for reply in self.replies]
+            tweetDict["replies"] = [reply.asJson for reply in self.replies]
         return json.dumps(tweetDict, indent=1)
         
 
 class Agent: 
-    def __init__(self, user:str, password:str) -> None:
+    def __init__(self, user:str='', password:str='') -> None:
         self.client:Twitter = Twitter("session")
-        self.client.sign_in(user, password)
+        if user and password:
+            self.client.sign_in(user, password)
         self.old_notifications = []
 
     @property
@@ -36,33 +38,47 @@ class Agent:
         for tweet in allTweets:
             try:
                 threads = [ConversationThread for ConversationThread in tweet.get_comments()]
-                allReplies = [tweet.expand() for tweet in threads]
+                allReplies = [tweet.expand() for tweet in threads] #type: ignore
                 if "poptimedev" not in [reply.author.username for item in allReplies for reply in item]:
                     nonRepliedTweets.append(tweet)
-            except:
+            except Exception as e:
+                with open("error.txt", "w") as f:
+                    f.write(str(e))
                 pass
                 
         nonRepliedTweets = [tweet for tweet in nonRepliedTweets if tweet not in self.old_notifications]
         self.old_notifications = nonRepliedTweets
         return nonRepliedTweets
     
-    def GetContextOfReply(self, tweet:Tweet) -> ArvoreTweets:
+    def GetContextOfReply(self, tweet:Tweet) -> ArvoreTweets|None:
         """
         Receives:
             - tweet (Tweet): The tweet to get the context from.
         Returns:
             - ArvoreTweets: A tree with the parents. Starts in the root of the conversation (the post) and ends in the reply (the tweet arg)
         """
+        
+        def build_tree(tweet: Tweet) -> ArvoreTweets:
+            tree = ArvoreTweets(tweet)
+            for reply in tweet.get_comments():
+                tree.replies.append(build_tree(reply))
+            return tree
         parentTweet = tweet.replied_to 
-        while parentTweet:
-            parentTweet = parentTweet.replied_to #type: ignore
+        print(parentTweet)
+        if parentTweet:
+            print('aa')
+            while parentTweet.replied_to:
+                parentTweet = parentTweet.replied_to
+            return build_tree(parentTweet)
         
 
 
         
     
 if __name__ == '__main__':
-    import json ; secrets = json.load(open('secrets.json'))
+    import json ; secrets = json.load(open('secrets.json', mode='r'))
     agent = Agent(secrets['twitter']['login'], secrets['twitter']['passwd'])
-    for tweet in agent.NewNotifications:
-        print(tweet.text, tweet.author.username, sep=' ---- ') #type: ignore
+    #agent = Agent()
+    notifications = agent.NewNotifications
+    print(notifications)
+    print(notifications[0].original_tweet, notifications[0].is_reply)
